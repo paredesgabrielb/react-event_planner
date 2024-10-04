57778fdf6f8a3a6afba0116d8a3e00cebcbcf5eb
paredesgabrielb@gmail.com <paredesgabrielb@gmail.com>
2024-10-04 17:36:25 -0400

    add footer.jsx add import in eventplanner.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; Gabriel Event Planner Organization. All rights reserved.</p>
+        </footer>
+    );
+}
+
+
+
+export default Footer;
